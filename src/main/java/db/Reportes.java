651f65185284a480d23e3f26d672c1c47b4f7572
Maria Azul Grande <azulegrande1@gmail.com>
2018-09-12 22:28:19 -0300

    subo reportes

--- src/main/java/db/Reportes.java
+++ src/main/java/db/Reportes.java
@@ -1,6 +1,7 @@
 package db;
 
-import java.time.LocalDate; 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,38 +14,43 @@ import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 
 import cliente.Cliente;
 import dispositivo.Dispositivo;
+import repositorio.RepoTransformadores;
 import transformador.Transformador;
 
 public class Reportes implements WithGlobalEntityManager{
 
 	EntityManager em = entityManager();
 	/*TODO Hay que ver que tipo devolverían los métodos*/
-//	public Map<Cliente, Double> consumoPorHogarEntre(LocalDate desde, LocalDate hasta) {
-//		Map<Cliente, Double> consumoPorHogar  = new HashMap <Cliente, Double>();
-//		em.createQuery("SELECT c.nombre, SUM(hc.consumo) consumoPorHogar FROM cliente c "
-//				+ "INNER JOIN dispositivo d ON d.id_cliente = c.id_cliente"
-//				+ "INNER JOIN historial_consumo hc ON hc.id_dispositivo = d.id" 
-//				+ "WHERE hc.fecha <= :desde AND hc.fecha >= :hasta"
-//				+ "GROUP BY d.id, c.id_cliente");
-//		  	 
-//		return consumoPorHogar;
-//	}
+	public Map<Cliente, Double> consumoPorHogarEntre(LocalDateTime desde, LocalDateTime hasta) {
+		Map<Cliente, Double> consumoPorHogar  = new HashMap <Cliente, Double>();
+		em.createQuery("SELECT c.nombre, SUM(hc.consumo) consumoPorHogar FROM cliente c "
+				+ "INNER JOIN dispositivo d ON d.id_cliente = c.id_cliente"
+				+ "INNER JOIN historial_consumo hc ON hc.id_dispositivo = d.id" 
+				+ "WHERE hc.fecha <= :desde AND hc.fecha >= :hasta"
+				+ "GROUP BY d.id, c.id_cliente").setParameter(0,desde).setParameter(1,hasta).getResultList();
+		  	 
+		return consumoPorHogar;
+	}
 	
-//	public Map<Dispositivo, Double> consumoPromedioPorDispositivoDe() {
-//		Map<Dispositivo, Double> consumoPromedioPorDispositivo  = new HashMap <Dispositivo, Double>();
-//		em.createQuery("SELECT c.nombre, d.nombre, AVG(hc.consumo) promedio"
-//		+ "FROM cliente c "
-//		+ "INNER JOIN dispositivo d ON d.id_cliente = c.id inner"
-//		+ "INNER JOIN historial_consumo hc ON hc.id_dispositivo = d.id"
-//		+ "GROUP BY d.id, c.id_cliente");
-//	
-//		return consumoPromedioPorDispositivo;
-//	}
-//	
-	/*public Map<Transformador, Double> consumoPorTransformadorEntre(LocalDate fechaInicial, LocalDate fechaFinal) {
+	public Map<Dispositivo, Double> consumoPromedioPorDispositivoDe() {
+		Map<Dispositivo, Double> consumoPromedioPorDispositivo  = new HashMap <Dispositivo, Double>();
+		em.createQuery("SELECT c.nombre, d.nombre, AVG(hc.consumo) promedio"
+				+ "FROM cliente c "
+				+ "INNER JOIN dispositivo d ON d.id_cliente = c.id inner"
+				+ "INNER JOIN historial_consumo hc ON hc.id_dispositivo = d.id"
+				+ "GROUP BY d.id, c.id_cliente").getResultList();
+	
+		return consumoPromedioPorDispositivo;
+	}
+	
+	public Map<Transformador, Double> consumoPorTransformadorEntre(LocalDateTime fechaInicial, LocalDateTime fechaFinal) {
 		Map<Transformador, Double> consumoPorTransformador  = new HashMap <Transformador, Double>();
-		return consumoPorTransformador.entrySet().stream().mapToDouble(transformador -> transformador.getKey().consumoEntre(fechaInicial, fechaFinal));
-	}*/
+/*		return consumoPorTransformador.entrySet().stream().mapToDouble(transformador -> 
+		transformador.getKey().consumoEntre(fechaInicial, fechaFinal));*/
+		RepoTransformadores.getInstance().obtenerTodas().forEach(
+				transformador -> consumoPorTransformador.put(transformador, transformador.consumoEntre(fechaInicial, fechaFinal)));
+		return consumoPorTransformador;
+	}
 	
 
 }
