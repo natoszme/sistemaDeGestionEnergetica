b31603b8aab846169af4c157bf76563b31f4e360
natoszme <natoszmedra@gmail.com>
2018-10-16 00:53:10 -0300

    Manejo en el controller las opciones de que el user no este identificado, sea admin o cliente. Hay que reveer, se repite logica

--- src/main/java/server/Controller.java
+++ src/main/java/server/Controller.java
@@ -10,31 +10,60 @@ import spark.Response;
 import usuario.Usuario;
 
 public class Controller {
+	
 	public static ModelAndView login(Request req, Response res) {
 		//TODO revisar el null
 		return new ModelAndView(null, "index.hbs");
 	}
 	
-	public static ModelAndView validarLogin(Request req, Response res) {		
-		Optional<Usuario> usuario = RepoUsuarios.getInstance().dameUsuario(req.attribute("usuario"), req.attribute("password"));
+	//TODO la pass tiene que llegar aca ya hasheada!
+	public static ModelAndView validarLogin(Request req, Response res) {
 		
-		return usuario.map(user -> modelAndViewSegun(user))
-				.orElse(new ModelAndView(null, "index.hbs"));
+		String username = req.queryParams("username");
+		String password = req.queryParams("password");
+		
+		Optional<Usuario> usuario = RepoUsuarios.getInstance().dameUsuario(username, password);
+		
+		System.out.println(usuario.isPresent());
+		
+		final HashMap<String, Object> viewModel = usuario.map(user -> viewModelSegun(user))
+				.orElse(viewModelConUsername(username));
+		
+		return usuario.map(user -> modelAndViewSegun(user, viewModel))
+				.orElse(new ModelAndView(viewModel, "index.hbs"));
 	}
 
-	private static ModelAndView modelAndViewSegun(Usuario user) {
+	private static HashMap<String, Object> viewModelConUsername(String username) {
+		HashMap<String, Object> viewModel = new HashMap<>();
+		viewModel.put("username", username);
+		return viewModel;
+	}
+
+	private static HashMap<String, Object> viewModelSegun(Usuario user) {
 		HashMap<String, Object> viewModel = new HashMap<>();
 		
-		return new ModelAndView(viewModel.put("usuario", user.getUser()),
+		if(user.esAdmin()) {
+			//TODO agregar las cosas del admin
+			//viewModel.put();
+		}
+		else {
+			viewModel.put("cliente", user.getCliente());
+		}
+		
+		return viewModel;
+	}
+
+	private static ModelAndView modelAndViewSegun(Usuario user, HashMap<String, Object> viewModel) {
+		//TODO llenar el viewModel con la data segun corresponda
+		return new ModelAndView(viewModel,
 				obtenerViewConPermisoDe(user));
 	}
 
 	private static String obtenerViewConPermisoDe(Usuario user) {
-		String recurso;
+		String recurso = "usuario";
 		if(user.esAdmin()) {
 			recurso = "admin";
 		}
-		recurso = "usuario";
 		return recurso + "/home.hbs";
 	}
 }
