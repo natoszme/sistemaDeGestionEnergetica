e3569634371034b26af528e9eab83dc152f681a0
natoszme <natoszmedra@gmail.com>
2018-10-16 00:51:29 -0300

    Vinculo al Usuario con el Cliente. Puede modelarse mejor?

--- src/main/java/usuario/Usuario.java
+++ src/main/java/usuario/Usuario.java
@@ -1,27 +1,38 @@
 package usuario;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToOne;
 
+import cliente.Cliente;
 import db.DatosBasicos;
 
 @Entity
 public class Usuario extends DatosBasicos{
-	private String usuario;
+	private String username;
 	private String password;
 	private boolean esAdmin;
 	
-	public Usuario(String usuario, String password, boolean esAdmin) {
-		this.usuario = usuario;
+	@OneToOne(fetch = FetchType.LAZY)
+	private Cliente cliente;
+	
+	public Usuario(String username, String password, boolean esAdmin, Cliente cliente) {
+		this.username = username;
 		this.password = password;
 		this.esAdmin = esAdmin;
+		this.cliente = cliente;
 	}
 	
-	public String getUser() {
-		return usuario;
+	public String getUsername() {
+		return username;
 	}
 
 	public boolean esAdmin() {
 		return esAdmin;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
 	}
 	
 }
