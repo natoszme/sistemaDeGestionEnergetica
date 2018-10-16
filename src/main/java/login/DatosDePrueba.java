2e6cc3dfe4083cd740d8863ce515071b45e4c471
natoszme <natoszmedra@gmail.com>
2018-10-16 13:37:31 -0300

    Mas datos de prueba

--- src/main/java/login/DatosDePrueba.java
+++ src/main/java/login/DatosDePrueba.java
@@ -2,16 +2,30 @@ package login;
 
 import javax.persistence.EntityManager;
 
+import org.uqbar.geodds.Point;
 import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
 import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
 
+import categoria.Categoria;
+import cliente.Cliente;
+import cliente.TipoDocumento;
 import usuario.Usuario;
 
 public class DatosDePrueba extends AbstractPersistenceTest implements WithGlobalEntityManager{
 	public void init() {
 		EntityManager em = entityManager();
 		
-		Usuario unUser = new Usuario("admin", "123", true, null);
-		withTransaction(() -> {em.persist(unUser); });
+		Point ubicacionLaMatanza = new Point(-34.762985, -58.631242);
+		Categoria r1 = new Categoria("R1", 0, 150, 18.76, 0.644);
+		
+		Usuario unAdmin = new Usuario("admin", "123", true, null);
+		Usuario unCliente = new Usuario("pepe", "1", false, new Cliente("Alejandro", "Saez", TipoDocumento.DNI, 3876675, 43543245, "Macos Sastre 324", r1, null, ubicacionLaMatanza));
+		
+		withTransaction(() -> {
+				em.persist(unAdmin);
+			
+				em.persist(r1);
+				em.persist(unCliente);
+			});
 	}
 }
