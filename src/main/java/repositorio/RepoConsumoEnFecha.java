d77c3ffcd0c2787b2fd2ccbd56e4c97bd34a395c
azul <azulegrande1@gmail.com>
2018-10-21 12:05:05 -0300

    Sigo laburando en el filtro de consumos por fecha de cliente. Tira error de permisos en la db

--- src/main/java/repositorio/RepoConsumoEnFecha.java
+++ src/main/java/repositorio/RepoConsumoEnFecha.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.uqbarproject.jpa.java8.extras.convert.LocalDateTimeConverter;
 import org.hibernate.Filter;
 
 import cliente.Cliente;
@@ -27,24 +28,30 @@ public class RepoConsumoEnFecha extends RepoEnDB<ConsumoEnFecha> {
 	}
 	
 	public List<ConsumoEnFecha> filtrarMedicionesXCliente(Cliente cliente, String desde, String hasta) { 
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 			
-		Session session = entityManager().unwrap(Session.class);
-		Filter filter = session.enableFilter("filtroDeFecha");
+		// Session session = entityManager().unwrap(Session.class);
+		// Filter filter = session.enableFilter("filtroDeFecha");
 		
-		filter.setParameter("desde", LocalDateTime.parse(desde, formatter));
-		filter.setParameter("hasta", LocalDateTime.parse(hasta, formatter));
+		// filter.setParameter("desde", LocalDateTime.parse(desde, formatter));
+		// filter.setParameter("hasta", LocalDateTime.parse(hasta, formatter));
 		
-		String query = "SELECT hc.* FROM Cliente c INNER JOIN  c.dispositivos AS d INNER JOIN  d.tipoDispositivo.consumosHastaElMomento AS hc";
-		query = "FROM ConsumoEnFecha";
-		List<ConsumoEnFecha> results = (List<ConsumoEnFecha>) session.createQuery(query);
+		String query = "SELECT hc "
+				+ "FROM Cliente c "
+				+ "INNER JOIN  c.dispositivos AS d "
+				+ "INNER JOIN  d.tipoDispositivo.consumosHastaElMomento AS hc "
+				+ "WHERE hc.fecha BETWEEN :desde AND :hasta";
+		
+		// query = "FROM ConsumoEnFecha";
+		List<ConsumoEnFecha> results = (List<ConsumoEnFecha>) entityManager()
+																.createQuery(query)
+																.setParameter("desde", LocalDateTime.parse(desde + " 00:00:00", formatter))
+																.setParameter("hasta", LocalDateTime.parse(hasta + " 23:59:59", formatter)).getResultList();
 		
 		for (ConsumoEnFecha c: results) {
 			System.out.println(c.getFecha());
 			System.out.println(c.getConsumo());
 		}
-		
-		session.disableFilter("filtroDeFecha");
 		
 		return results;
 	}
