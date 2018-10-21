120f98c5cf602e4ebde3e0d9ef9fb93ac9b0bc83
Kevin Szuchet <kevinszuchet@hotmail.com>
2018-10-21 16:06:14 -0300

    Agrego metodos al parser para parsear a partir de un string o de un objeto (o lista)

--- src/main/java/json/JSONParser.java
+++ src/main/java/json/JSONParser.java
@@ -11,15 +11,13 @@ import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
-public class JSONParser<Entidad> {		 
+public class JSONParser<Entidad> {
+	
+	ObjectMapper mapper = new ObjectMapper().registerModule(new ParameterNamesModule()).registerModule(new Jdk8Module()).registerModule(new JavaTimeModule());
 	
 	public List<Entidad> importar(String rutaArchivo, Class<Entidad> tipoEntidad) {	
 		
 		File archivoJson = new File(rutaArchivo);
-		ObjectMapper mapper = new ObjectMapper()
-					.registerModule(new ParameterNamesModule())
-					.registerModule(new Jdk8Module())
-					.registerModule(new JavaTimeModule());
 		List<Entidad> importados = new ArrayList<>();
 		
 		try {
@@ -33,5 +31,56 @@ public class JSONParser<Entidad> {
 		}
 		
 		return importados;
+	}
+	
+	public Entidad jsonToObject(String json, Class<Entidad> tipoEntidad) {
+		
+		Entidad elemento = null;
+		
+		try {
+			elemento = mapper.readValue(json, tipoEntidad);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return elemento;
+	}
+	
+	public String objectToJson(Entidad entidad) {
+
+		String json = ""; 
+		
+		try {
+			json = mapper.writeValueAsString(entidad);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return json;
+	}
+	
+	public String listToJson(List<Entidad> entidad) {
+
+		String json = ""; 
+		
+		try {
+			json = mapper.writeValueAsString(entidad);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return json;
 	}
 }
