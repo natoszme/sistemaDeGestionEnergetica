d5d68d6b1e0e5677ba9185dcaf1d8bcaa6e3934d
Kevin Szuchet <kevinszuchet@hotmail.com>
2018-10-21 16:08:57 -0300

    Se muestran las mediciones filtradas por ajax (falta arreglar el formateo de fechas en el repo de consumos)

--- src/main/java/repositorio/RepoConsumoEnFecha.java
+++ src/main/java/repositorio/RepoConsumoEnFecha.java
@@ -2,11 +2,10 @@ package repositorio;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.Session;
-import org.uqbarproject.jpa.java8.extras.convert.LocalDateTimeConverter;
-import org.hibernate.Filter;
+import javax.persistence.Query;
 
 import cliente.Cliente;
 import repositorio.RepoEnDB;
@@ -29,30 +28,37 @@ public class RepoConsumoEnFecha extends RepoEnDB<ConsumoEnFecha> {
 	
 	public List<ConsumoEnFecha> filtrarMedicionesXCliente(Cliente cliente, String desde, String hasta) { 
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-			
-		// Session session = entityManager().unwrap(Session.class);
-		// Filter filter = session.enableFilter("filtroDeFecha");
 		
-		// filter.setParameter("desde", LocalDateTime.parse(desde, formatter));
-		// filter.setParameter("hasta", LocalDateTime.parse(hasta, formatter));
+		Query query = entityManager()
+						.createQuery(
+							"SELECT hc.fecha AS fecha, hc.consumo AS consumo "
+							+ "FROM Cliente c "
+							+ "INNER JOIN  c.dispositivos AS d "
+							+ "INNER JOIN d.tipoDispositivo td "
+							+ "INNER JOIN  td.consumosHastaElMomento AS hc "
+							+ "WHERE c = :cliente "
+							+ (!desde.isEmpty() ? "AND hc.fecha >= :desde " : "")
+							+ (!hasta.isEmpty() ? "AND hc.fecha <= :hasta" : "")
+						);
 		
-		String query = "SELECT hc "
-				+ "FROM Cliente c "
-				+ "INNER JOIN  c.dispositivos AS d "
-				+ "INNER JOIN  d.tipoDispositivo.consumosHastaElMomento AS hc "
-				+ "WHERE hc.fecha BETWEEN :desde AND :hasta";
+		query.setParameter("cliente", cliente);
 		
-		// query = "FROM ConsumoEnFecha";
-		List<ConsumoEnFecha> results = (List<ConsumoEnFecha>) entityManager()
-																.createQuery(query)
-																.setParameter("desde", LocalDateTime.parse(desde + " 00:00:00", formatter))
-																.setParameter("hasta", LocalDateTime.parse(hasta + " 23:59:59", formatter)).getResultList();
-		
-		for (ConsumoEnFecha c: results) {
-			System.out.println(c.getFecha());
-			System.out.println(c.getConsumo());
+		if (!desde.isEmpty()) {
+			query.setParameter("desde", LocalDateTime.parse(desde + " 00:00:00", formatter));
 		}
 		
-		return results;
+		if (!hasta.isEmpty()) {
+			query.setParameter("hasta", LocalDateTime.parse(hasta + " 23:59:59", formatter)).getResultList();
+		}
+		
+		List<Object[]> results = query.getResultList();
+		List<ConsumoEnFecha> mediciones = new ArrayList<ConsumoEnFecha>();
+		
+		results.stream().forEach(result -> {
+			mediciones.add(new ConsumoEnFecha((LocalDateTime) result[0], (Double) result[1]));
+			
+		});
+		
+		return mediciones;
 	}
 }
