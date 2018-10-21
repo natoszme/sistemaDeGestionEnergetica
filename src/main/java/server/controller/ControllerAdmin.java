6d5ac925584fbdf4d70ace7e6184da426c9f9c02
natoszme <natoszmedra@gmail.com>
2018-10-21 11:59:55 -0300

    Persisto el dispositivo generico creado en un repo dedicado. Falta volar la clase DispositivoBaseFactory

--- src/main/java/server/controller/ControllerAdmin.java
+++ src/main/java/server/controller/ControllerAdmin.java
@@ -2,11 +2,14 @@ package server.controller;
 
 import java.util.HashMap;
 
+import dispositivo.Dispositivo;
 import server.login.Autenticable;
 import server.login.RepoAdmins;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
+import tipoDispositivo.DispositivoEstandar;
+import tipoDispositivo.DispositivoInteligente;
 
 public class ControllerAdmin extends ControllerLogin{
 
@@ -37,6 +40,22 @@ public class ControllerAdmin extends ControllerLogin{
 	}
 	
 	public String crearDispositivo(Request req, Response res) {
+		
+		String nombre = req.queryParams("nombre");
+		double kwPorHora = Double.parseDouble(req.queryParams("consumo"));
+		String tipoDispositivo = req.queryParams("tipoDispositivo");
+		
+		Dispositivo dispositivo;
+		
+		if(tipoDispositivo.equals("estandar")) {
+			dispositivo = new Dispositivo(nombre, new DispositivoEstandar(), kwPorHora);
+		}
+		else {
+			dispositivo = new Dispositivo(nombre, new DispositivoInteligente(null), kwPorHora);
+		}
+		
+		RepoDispositivosBase.getInstance().agregarEntidad(dispositivo);
+		
 		res.redirect("/admin/home");
 		return null;
 	}
---
+++ src/main/java/server/controller/RepoDispositivosBase.java
@@ -0,0 +1,21 @@
+package server.controller;
+
+import dispositivo.Dispositivo;
+import repositorio.RepoEnDB;
+
+public class RepoDispositivosBase extends RepoEnDB<Dispositivo>{
+	
+	private static RepoDispositivosBase instancia;
+
+	public RepoDispositivosBase(String tabla) {
+		super(tabla);
+	}
+	
+	public static RepoDispositivosBase getInstance() {
+		if(instancia == null) {
+			instancia = new RepoDispositivosBase("Dispositivo");
+		}
+		return instancia;
+	}
+
+}
