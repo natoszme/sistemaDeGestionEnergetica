3cda3f4cf031435a220407b41fc592913208938f
Nicolas Feldfeber <nicofeldfeber@gmail.com>
2018-10-24 16:01:19 -0300

    Agrego el helper que obtiene el consumo dado un cliente y un rango de fecha

--- src/main/java/spark/utils/HandlebarsTemplateEngineBuilder.java
+++ src/main/java/spark/utils/HandlebarsTemplateEngineBuilder.java
@@ -2,12 +2,20 @@ package spark.utils;
 
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Helper;
 import com.github.jknack.handlebars.HumanizeHelper;
+import com.github.jknack.handlebars.Options;
 import com.github.jknack.handlebars.helper.I18nHelper;
 import com.github.jknack.handlebars.helper.NumberHelper;
 import com.github.jknack.handlebars.helper.StringHelpers;
+
+import cliente.Cliente;
+import repositorio.RepoConsumoEnFecha;
 
 public class HandlebarsTemplateEngineBuilder {
 
@@ -39,6 +47,39 @@ public class HandlebarsTemplateEngineBuilder {
 		NumberHelper.register(getHandlerbars());
 		HumanizeHelper.register(getHandlerbars());
 		withHelper("i18n", I18nHelper.i18n);
+		
+		/*Handlebars handlebars = new Handlebars();
+		handlebars.registerHelper("consumo", new Helper<Cliente>() {
+			
+			@Override
+			public Object apply(Cliente cliente, Options arg1) throws IOException {
+				//LocalDateTime desde = formatearFecha(req.queryParams("desde"), LocalTime.of(0, 0, 0, 0));
+				//LocalDateTime hasta = formatearFecha(req.queryParams("hasta"), LocalTime.of(23, 59, 59, 999));
+				Double consumo =RepoConsumoEnFecha.getInstance()
+						.obtenerConsumoDeClienteEnFecha(cliente, (LocalDateTime)arg1.param(0),(LocalDateTime) arg1.param(1));
+				System.out.println("Consumo del cliente " + cliente.getNombre() + " = " + consumo);
+				return consumo;
+			}
+			});*/
+		Helper<Cliente> helperConsumoEnFecha = new Helper<Cliente>() {
+			@Override
+			public Object apply(Cliente cliente, Options arg1) throws IOException {
+				//LocalDateTime desde = formatearFecha(req.queryParams("desde"), LocalTime.of(0, 0, 0, 0));
+				//LocalDateTime hasta = formatearFecha(req.queryParams("hasta"), LocalTime.of(23, 59, 59, 999));
+				Double consumo =RepoConsumoEnFecha.getInstance()
+						.obtenerConsumoDeClienteEnFecha(cliente, (LocalDateTime)arg1.param(0),(LocalDateTime) arg1.param(1));
+				System.out.println("Consumo del cliente " + cliente.getNombre() + " = " + consumo);
+				return consumo;
+			}
+		};
+		withHelper("consumo", helperConsumoEnFecha);
+		
+		
+		
+		
 		return this;
 	}
+	
+	
+	
 }
