dc3e56c311604a28c4ed308e81a5dc77af9f9160
Nicolas Feldfeber <nicofeldfeber@gmail.com>
2018-10-24 16:03:00 -0300

    Modifico el viewModel y el .hbs para que traigan los datos de la base de datos

--- src/main/java/server/controller/ControllerAdmin.java
+++ src/main/java/server/controller/ControllerAdmin.java
@@ -1,15 +1,26 @@
 package server.controller;
 
 import java.awt.Point;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Helper;
+import com.github.jknack.handlebars.HumanizeHelper;
+import com.github.jknack.handlebars.Options;
 
 import cliente.Cliente;
 import cliente.TipoDocumento;
 import dispositivo.Dispositivo;
 import repositorio.RepoCategorias;
 import repositorio.RepoClientes;
+import repositorio.RepoConsumoEnFecha;
 import server.login.Autenticable;
 import server.login.RepoAdmins;
 import spark.ModelAndView;
@@ -20,32 +31,50 @@ import tipoDispositivo.DispositivoInteligente;
 
 public class ControllerAdmin extends ControllerLogin{
 
+	
+	
 	public ModelAndView home(Request req, Response res) {
 		
-		String fechaDesde = req.queryParams("desde"); 
-		String fechaHasta = req.queryParams("hasta"); 
+		LocalDateTime desde = null;
+		LocalDateTime hasta = null;
+		//LocalDateTime desde = formatearFecha(req.queryParams("desde"), LocalTime.of(0, 0, 0, 0));
+		//LocalDateTime hasta = formatearFecha(req.queryParams("hasta"), LocalTime.of(23, 59, 59, 999));
 		
 		HashMap<String, Object> viewModel = new HashMap<>();
+	
+		List<Cliente> clientes = RepoClientes.getInstance().obtenerTodas();
 		
-		//List<Cliente> clientes = RepoClientes.getInstance().obtenerTodas();
-		List<Cliente> clientes= new ArrayList<>();
+		viewModel.put("clientes", clientes);
+		viewModel.put("fechaDesde", desde);
+		viewModel.put("fechaHasta", hasta);
+		
+		return new ModelAndView(viewModel, "admin/home.hbs");
+		
+		
+		
+		/*List<Cliente> clientes= new ArrayList<>();
 		List<Dispositivo> dispositivos = new ArrayList<>();
 		Cliente clienteEjemplo = new Cliente("Bebe","1","Bebe", "Perro", TipoDocumento.DNI,(long)11111111, (long)1123894928, "Casa de nato", RepoCategorias.getInstance().obtenerCategoriaPorNombre("R1"), dispositivos, new org.uqbar.geodds.Point(0,1));
 		clientes.add(clienteEjemplo);
-		
-		
 		List<Double> consumoPorCliente= new ArrayList<>();
 		consumoPorCliente.add(100.2);
-		
-		HashMap<Cliente, Double> clienteConConsumo;
+		HashMap<Cliente, Double> clienteConConsumo;*/
 		//clienteConConsumo.put(key, value)
 		//clientes.forEach(cliente->clientesConConsumo.pu);
-		
 		//clientes.forEach(cliente->consumoPorCliente.add(cliente.consumoRealizadoEntre(fechaInicial, fechaFinal)));
-		
 		//viewModel.put("consumos", //lo que devuelva el reporte);
 		
-		return new ModelAndView(viewModel, "admin/home.hbs");
+		
+	}
+	
+	public double consumoDeClienteEnFecha(Cliente cliente, LocalDateTime desde, LocalDateTime hasta) {
+		return RepoConsumoEnFecha.getInstance().obtenerConsumoDeClienteEnFecha(cliente, desde, hasta);
+	}
+	
+	//TODO pasar esto arriba y que admin y cliente lo usen
+	private LocalDateTime formatearFecha(String fecha, LocalTime tiempo) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		return (!fecha.isEmpty() ? LocalDateTime.of(LocalDate.parse(fecha, formatter), tiempo) : null);		
 	}
 
 	protected String home() {
