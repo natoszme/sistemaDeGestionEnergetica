43f3ccb3d4258cd7eff8bda5717af0334314f390
Nicolas Feldfeber <nicofeldfeber@gmail.com>
2018-10-25 00:38:25 -0300

    Elimino el helper propio ya que no se usa mas

--- src/main/java/spark/utils/HandlebarsTemplateEngineBuilder.java
+++ src/main/java/spark/utils/HandlebarsTemplateEngineBuilder.java
@@ -1,10 +1,12 @@
 package spark.utils;
 
 import spark.template.handlebars.HandlebarsTemplateEngine;
+import tipoDispositivo.ConsumoEnFecha;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
 
 import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.Helper;
@@ -61,18 +63,21 @@ public class HandlebarsTemplateEngineBuilder {
 				return consumo;
 			}
 			});*/
-		Helper<Cliente> helperConsumoEnFecha = new Helper<Cliente>() {
+		/*Helper<Cliente> helperConsumoEnFecha = new Helper<Cliente>() {
 			@Override
 			public Object apply(Cliente cliente, Options arg1) throws IOException {
 				//LocalDateTime desde = formatearFecha(req.queryParams("desde"), LocalTime.of(0, 0, 0, 0));
 				//LocalDateTime hasta = formatearFecha(req.queryParams("hasta"), LocalTime.of(23, 59, 59, 999));
-				Double consumo =RepoConsumoEnFecha.getInstance()
+				Double consumo = RepoConsumoEnFecha.getInstance()
 						.obtenerConsumoDeClienteEnFecha(cliente, (LocalDateTime)arg1.param(0),(LocalDateTime) arg1.param(1));
+				
+			
+				
 				System.out.println("Consumo del cliente " + cliente.getNombre() + " = " + consumo);
 				return consumo;
 			}
 		};
-		withHelper("consumo", helperConsumoEnFecha);
+		withHelper("consumo", helperConsumoEnFecha);*/
 		
 		
 		
