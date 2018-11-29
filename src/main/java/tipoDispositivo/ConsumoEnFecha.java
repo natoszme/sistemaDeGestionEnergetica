2a981d9360021f04f83103fc83d5ebd7d6e0d229
Kevin Szuchet <kevinszuchet@hotmail.com>
2018-11-29 17:23:30 -0300

    Saco annotations que no sirven, arreglo un tema de espacios en una query

--- src/main/java/tipoDispositivo/ConsumoEnFecha.java
+++ src/main/java/tipoDispositivo/ConsumoEnFecha.java
@@ -6,25 +6,16 @@ import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.Embeddable;
 
-import org.hibernate.annotations.Filter;
-import org.hibernate.annotations.FilterDef;
-import org.hibernate.annotations.ParamDef;
-
 import org.uqbarproject.jpa.java8.extras.convert.LocalDateTimeConverter;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 
 @Embeddable
-@FilterDef(
-	name = "filtroDeFecha", 
-	parameters = { @ParamDef(name = "desde", type = "datetime"), @ParamDef(name = "hasta", type = "datetime") }
-)
 public class ConsumoEnFecha {
 	
 	@Column(nullable = false)
 	@Convert(converter = LocalDateTimeConverter.class)
-    @Filter(name = "filtroDeFecha", condition = "fecha BETWEEN :desde AND :hasta")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
 	private LocalDateTime fecha;
 	
